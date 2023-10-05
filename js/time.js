2cd4cc58c6b2a42c4aa41026707cf614dd407061
Mxmilu <mxmilu666@gmail.com>
2023-10-05 16:35:11 +0800

    修复time.js导致的不显示时间问题

--- js/time.js
+++ js/time.js
@@ -12,15 +12,14 @@ function time() {
     var todayHour = today.getHours()
     var todayMinute = today.getMinutes()
     var todaySecond = today.getSeconds()
-    var t1 = Date.UTC(2021, 7, 9, 00, 00, 00)
+    var t1 = Date.UTC(2021, 7, 9, 0, 0, 0)
     var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
     var diff = t2 - t1
     var diffYears = Math.floor(diff / years)
     var diffDays = Math.floor((diff / days) - diffYears * 365)
     var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours)
     var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
-    var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
-        minutes) / seconds)
+    var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds)
     document.getElementById("time").innerHTML = diffYears + "年" + diffDays + "天" + diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒"
 }
 time()
